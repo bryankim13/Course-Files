0a85cf619a43d791297bb73506ee37b967aff015
Bryan Kim <bryankim13173@gmail.com>
2019-12-13 17:44:13 -0500

    postlab 5

---
+++ Program and Data Representation/AVL Tree/AVLNode.h
@@ -0,0 +1,23 @@
+// Bryan Kim
+// bjk3yf
+// 10/14/19
+// AVLNode.h
+
+#ifndef AVLNODE_H
+#define AVLNODE_H
+#include <string>
+using namespace std;
+
+class AVLNode {
+  AVLNode();
+  ~AVLNode();
+
+  string value;
+  AVLNode* left;
+  AVLNode* right;
+  int height;
+
+  friend class AVLTree;
+};
+
+#endif
---
+++ Program and Data Representation/AVL Tree/AVLPathTest.cpp
@@ -0,0 +1,25 @@
+// Bryan Kim
+// bjk3yf
+// 10.14.19
+// AVLPathTest.cpp
+#include "AVLTree.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  AVLTree avl;
+  while (cin.good()) {
+    string instr, word;
+    cin >> instr;
+    cin >> word;
+    if (instr == "I") {
+      avl.insert(word);
+    } else if (instr == "R") {
+      avl.remove(word);
+    } else if (instr == "L") {
+      cout << "AVL path: " << avl.pathTo(word) << endl;
+    }
+  }
+  cout << "AVL numNodes: " << avl.numNodes() << endl;
+}
